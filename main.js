9c013730a66f60d073471e501256e56656e81be0
N-ishant <nishantupreti30@gmail.com>
2023-10-16 03:52:14 +0530

    Updating the user details

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function onSubmit(e){
 
         //Sending a POST Request to CRUD API
         axios
-          .post("https://crudcrud.com/api/7e2375639d7445d492404b3e4176dc41/appointmentData" , user)
+          .post("https://crudcrud.com/api/54e8369e190948619632674b81462105/appointmentData" , user)
           .then((res) => {
             showUserOnScreen(res.data)
             console.log(res);
@@ -39,13 +39,13 @@ function onSubmit(e){
             document.body.innerHTML = document.body.innerHTML + "<h4>Something went wrong</h4>"
             console.log(error);
           }) 
-    }
+    } 
 }
 
 //Sending a GET Request to CRUD API
 window.addEventListener("DOMContentLoaded" , () => {
     axios
-      .get("https://crudcrud.com/api/7e2375639d7445d492404b3e4176dc41/appointmentData")
+      .get("https://crudcrud.com/api/54e8369e190948619632674b81462105/appointmentData")
       .then((res) => {
         console.log(res)
         for(var i=0;i<res.data.length;i++){
@@ -66,7 +66,7 @@ function showUserOnScreen(user){
     deleteBtn.onclick = () => {
         //Sending a DELETE Request to CRUD API
         axios
-          .delete(`https://crudcrud.com/api/7e2375639d7445d492404b3e4176dc41/appointmentData/${user._id}`)
+          .delete(`https://crudcrud.com/api/54e8369e190948619632674b81462105/appointmentData/${user._id}`)
           .then((res) => {
             userList.removeChild(li);
           })
@@ -82,6 +82,38 @@ function showUserOnScreen(user){
         document.getElementById('name').value = user.name;
         document.getElementById('email').value = user.email;
         document.getElementById('mobile').value = user.mobile;
+ 
+        //Replace the Existing Event Listener with new one
+        myForm.removeEventListener('submit', onSubmit);
+
+        myForm.addEventListener('submit' , (e) => {
+            e.preventDefault();
+
+            const updatedUser = {
+                name: document.getElementById('name').value,
+                email: document.getElementById('email').value,
+                mobile: document.getElementById('mobile').value
+            };
+    
+            axios
+              .put(`https://crudcrud.com/api/54e8369e190948619632674b81462105/appointmentData/${user._id}`, updatedUser)
+              .then((res) => {
+                //Update the user object
+                user.name = updatedUser.name;
+                user.email = updatedUser.email;
+                user.mobile = updatedUser.mobile;
+                //Update the details text node
+                details.nodeValue = `${user.name} : ${user.email} : ${user.mobile}`;
+                //Clear the form after updating
+                nameInput.value = '';
+                emailInput.value = '';
+                mobileInput.value = '';
+              })
+              .catch((error) => console.log(error))
+
+              //Restore the original Event Listener
+              myForm.addEventListener('submit' , onSubmit);
+        })
     }
 
     li.appendChild(details);
